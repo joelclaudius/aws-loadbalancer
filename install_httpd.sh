4957058e9551877c59fc60f13603290f988b61ed
joelclaudius <92925712+joelclaudius@users.noreply.github.com>
2023-11-13 18:54:53 +0300

    Update install_httpd.sh

--- install_httpd.sh
+++ install_httpd.sh
@@ -4,4 +4,4 @@ yum update -y
 yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
-echo "<html><h1><p> Welcome to Utrains.<br> You are redirected to ${HOSTNAME} to see how the load balancer is sharing the traffic.</p></h1></html>" > /var/www/html/index.html
+echo "<html><h1><p> Welcome to ctech.ink.<br> You are redirected to ${HOSTNAME} to see how the load balancer is sharing the traffic.</p></h1></html>" > /var/www/html/index.html
